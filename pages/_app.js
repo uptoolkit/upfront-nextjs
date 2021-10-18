8b11c01f99abdf794259ccdc1dfd828c4debfd0a
danielsum <daniel@lacerisenumerique.be>
2021-10-18 08:18:32 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,37 @@
 import '../styles/globals.css'
+import {useUp} from "use-up";
 
-function MyApp({ Component, pageProps }) {
+if (useUp) {
+  useUp({
+    debug: true,
+    project : {
+      name: 'Up Toolkit Demo',
+      logo: {
+        src: '/img/logo.svg',
+      },
+      url: '/'
+    },
+    storeMode: 'reactive', // could be reactive|vuex
+    // store: store, // if defined you can define your vuex store if you choose vuex
+    api: {
+      url: 'https://uptoolkit/demo/api', // Replace with your api endpoint
+    },
+    translations: {
+      en_EN: {
+        hello: 'Hello World !',
+      },
+      fr_FR: {
+        hello: 'Bonjour le monde',
+      },
+    },
+    locale: 'en_EN',
+    locales: [
+      'en_EN', 'fr_FR'
+    ],
+  });
+}
+
+function MyApp({Component, pageProps}) {
   return <Component {...pageProps} />
 }
 
